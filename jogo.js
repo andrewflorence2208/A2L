bd2cd7ffa5cfbc1a268b9bbd8996a65b1bfb5115
arthurAL_2022 <115472995+aaljim@users.noreply.github.com>
2023-10-30 10:54:52 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,4 +1,21 @@
 var score = 0
+var gamestatus = 0
+function reload(){
+    score += 100
+    draw();
+    ctx.clearRect()}
+
+    var player = {
+        speed: 256,
+        width: 50,
+        height: 50,
+        lifes: 3,
+        key_pieces: 0 
+        }
+    var tela_quiz = {
+            pergunta: {},
+            respostas: {}
+        }
 function draw() {
 var canvas  = document.getElementById("tela");
 canvas.width = 800;
@@ -12,35 +29,24 @@ var button2c = [Math.floor(Math.random() * 750), Math.floor(Math.random() * 750)
 var button3c = [Math.floor(Math.random() * 750), Math.floor(Math.random() * 750)]
 var doorc = [Math.floor(Math.random() * 750), Math.floor(Math.random() * 750)]
 
-var buttonsxc = [button1c, button2c, button3c, doorc]
-console.log(buttonsxc)
 
 
-var tela_quiz = {
-    pergunta: {},
-    respostas: {}
-}
 document.getElementById("tela_quiz").innerHTML = score
 
-var player = {
-speed: 256,
-width: 50,
-height: 50,
-lifes: 3,
-key_pieces: 0 
-}
+
 player.x = canvas.width/2 - player.width/2;
 player.y = canvas.height/2 - player.height/2;
 player.currentx = 0
 player.currenty = 0
 
 class square  {
-    constructor(x, y, width, height, squarecolor) {
+    constructor(x, y, width, height, squarecolor, status) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height
         this.color = squarecolor
+        this.status = status
     }
     drawSide() {
     ctx.fillStyle = this.color
@@ -61,12 +67,25 @@ class button  {
 
 
 var buttons = [new button(button1c[0], button1c[1], 50, 50, 0), new button(button2c[0], button2c[1], 50, 50, 0), new button(button3c[0], button3c[1], 50, 50, 0)]
-console.log(buttons)
+for (i = 1; i < buttons.length; i++ ) {
+    if (buttons[i].x == player.x || buttons[i].y == player.y) {
+        buttons[i].x = Math.floor(Math.random() * 750)
+        buttons[i].y = Math.floor(Math.random() * 750)
+    }
+    if (buttons[i].x == buttons[i - 1].x || buttons[i].y == buttons[i - 1].y){
+        buttons[i].x = Math.floor(Math.random() * 750)
+        buttons[i].y = Math.floor(Math.random() * 750)
+    }
+    if (i == 3) {
+        if (buttons[i].x == buttons[i - 2].x || buttons[i].y == buttons[i - 2].y){
+        buttons[i].x = Math.floor(Math.random() * 750)
+        buttons[i].y = Math.floor(Math.random() * 750) 
+    }}
+    
+}
 
-var door = new square(doorc[0], doorc[1], 50, 50, "yellow")
+var door = new square(doorc[0], doorc[1], 50, 50, "yellow", 0)
 
-var map = [new square(0, 0, 50, 50), new square(0, 350, 50, 50)]
-console.log(map)
 
 var collision = function(){
 var color = "black";
@@ -77,25 +96,25 @@ for (var i = 0; i < buttons.length; i++){
         player.y+player.height >= buttons[i].y){
             color = "red"
             buttons[i].status = 1,
-            player.key_pieces += 1
             buttons[i].x = -50
             buttons[i].y = -50
-            console.log(i, buttons[i].status)
-            document.getElementById("tela_quiz").innerHTML = score + " " + "TESTE" + " " + player.lifes + " " + player.key_pieces
+            document.getElementById("tela_quiz").innerHTML = score + " " + "TESTE" + " " + player.lifes + " " + player.key_pieces + " " + gamestatus
+            quiz()
         }
+}
+if (player.key_pieces == 3) {
+    door.status = 1
+} else {
+    door.status = 0
 }
     if(player.x+player.width >= door.x && 
         player.x <= door.x+door.width && 
         player.y <= door.y+door.height && 
-        player.y+player.height >= door.y){
-            score += 100
-            document.getElementById("tela_quiz").innerHTML = score + " " + "TESTE" + " " + player.lifes + " " + player.key_pieces
-            draw()
+        player.y+player.height >= door.y && door.status == 1 ){
+            document.getElementById("tela_quiz").innerHTML = score + " " + "TESTE" + " " + player.lifes + " " + player.key_pieces + " " + gamestatus
+            reload()
         }
-        
-           
         ctx.fillStyle = color;
-    
 }
 
 var keysDown = {};
@@ -124,7 +143,6 @@ var update = function (modifier) {
         player.x += player.speed * modifier;
     }
     
-   
 };
 
 
@@ -138,23 +156,22 @@ function render() {
         buttons[i].drawSide()
         }
         }
-    if (buttons[0].status == 1 && buttons[1].status == 1 && buttons[2].status == 1){
+    if (door.status == 1){
     door.drawSide()}
 }
 
 
 var main = function () {
     update(0.02);
-    render();
     collision()
+    render()
     requestAnimationFrame(main);
 }
 
 main();
 }
 
+
 function gamestart() {
     document.getElementById("menu").style.display = "none";
-    document.getElementById("game").style.display = "inline";
-    draw();
-}
+    document.getElementById("game").style.display = "inline";}
