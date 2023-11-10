765a5cb265e06bbab6ffb0a47a4beb13f8d18ddb
Gin <115474107+GinKatsumi@users.noreply.github.com>
2023-11-10 15:54:27 -0300

    Add files via upload

---
+++ musiquinhas.js
@@ -0,0 +1,19 @@
+var musgas = [
+    'bitmusga.mp3',
+    'quenteleite.mp3',
+];
+
+var indiceAtual = 0;
+
+function nextSong() {
+    indiceAtual++;
+    if (indiceAtual === musgas.length) {
+        indiceAtual = 0;
+    }
+    document.getElementById('chumbo').src = musgas[indiceAtual];
+    document.getElementById('chumbo').play();
+}
+
+window.addEventListener('load', function () {
+    document.getElementById('chumbo').volume = 0.2;
+});
