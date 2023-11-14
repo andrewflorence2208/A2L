c1118fe316b58cf25b4e86f91153848f4e72662d
arthurAL_2022 <115472995+aaljim@users.noreply.github.com>
2023-11-14 13:14:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function tutorial(){
 };
 
 const tutorial_text = [
-    "O objetivo do jogo é finalizar labirintos ao responder questionários que abrem a porta para o próximo labirinto, cada labirinto tem 3 questionários (botões azuis) e 1 porta (botão amarelo), que precisam ser encontrados movimentando o personagem principal",
+    "O objetivo do jogo é finalizar labirintos ao responder questionários que abrem a porta para o próximo labirinto, cada labirinto tem 3 questionários (botões azuis) e 1 porta (botão amarelo), que precisam ser encontrados.",
     "O personagem se move nas 4 direções cardinais e as diagonais, controlado pelas setas cima, baixo, esquerda e direita ou W, S, A, D no teclado, respectivamente",
     "Os questionários são ativados ao mover o personagem por cima dos botões azuir",
     "Há apenas uma resposta correta para cada questão, você possui 3 vidas e perde 1 para cada resposta errada selecionada, perdendo toda sua pontuação e voltando a tela inicial caso as vidas acabem"
